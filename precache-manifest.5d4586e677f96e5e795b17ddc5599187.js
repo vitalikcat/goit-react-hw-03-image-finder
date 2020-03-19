08aaa1b96357404c578b01c634602864413bcefa
Vitalik <vitalik791@gmail.com>
2020-03-19 15:56:21 +0200

    Updates

---
+++ precache-manifest.5d4586e677f96e5e795b17ddc5599187.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4bd66876522498f25aad36ec823bc5b",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "67e178452172bee99b7f",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "a49a59105b604c944f34",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.ff25bca9.chunk.css"
+  },
+  {
+    "revision": "67e178452172bee99b7f",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b1c7db0b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b1c7db0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a49a59105b604c944f34",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.66965d17.chunk.js"
+  },
+  {
+    "revision": "ae3a2cafd41dba4ace62",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.3da2d935.js"
+  }
+]);
